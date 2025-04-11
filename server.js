0fe7bf2d1eaa7c733cccc43bce2c0b037ed9ce57
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-11 11:48:19 +0530

    add endpoint to retrieve user details

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/api/get", (req, res) => {
   res.json({ message: "Hello back to Nodejs" });
 });
 
+app.get("/api/get_user_details", (req, res) => {
+  res.json({ name: "John Doe", age: 30 });
+});
+
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
