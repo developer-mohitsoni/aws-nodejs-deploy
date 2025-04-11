2fa9812f4079f7726e0bf6d241944a62b29c2cbd
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-11 12:11:59 +0530

    Update server.js to use environment variable for user name and modify node.js.yml for improved workflow

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ import express from "express";
 
 const app = express();
 
+const port = process.env.PORT;
+
 app.get("/api/get", (req, res) => {
   res.json({ message: "Hello back to Nodejs" });
 });
 
 app.get("/api/get_user_details", (req, res) => {
-  res.json({ name: "John Doe", age: 30 });
+  res.json({ name: process.env.NAME, age: 30 });
 });
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 8000");
 });
